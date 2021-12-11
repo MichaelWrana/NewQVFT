4d4ee9042f5bda038e8b170ef89255565d70def3
MichaelWrana <wranamichael@gmail.com>
2021-12-10 23:59:05 -0500

    added data file

---
+++ qvft/data.py
@@ -0,0 +1,131 @@
+nav_data = [
+    {
+        "caption": "What We Do",
+        "href": "/about"
+    },
+    {
+        "caption": "Our Team",
+        "href": "/team"
+    },
+    {
+        "caption": "Sponsorship",
+        "href": "/sponsors"
+    },
+    {
+        "caption": "Hiring",
+        "href": "/hiring"
+    }
+]
+
+team_data = [
+    {
+        "name": "Patrick Singal",
+        "role": "Manager",
+        "img": "patrick.webp"
+    },
+    {
+        "name": "Chris Molloy",
+        "role": "Database Developer",
+        "img": "chris.webp"
+    },
+    {
+        "name": "Iain Headrick",
+        "role": "Mechanical Designer",
+        "img": "iain.webp"
+    },
+    {
+        "name": "Julia Mackey",
+        "role": "Business Operations",
+        "img": "julia.webp"
+    },
+    {
+        "name": "Ryan Power",
+        "role": "Hardware Designer",
+        "img": "ryan.webp"
+    },
+    {
+        "name": "Elissa Wong",
+        "role": "Plant Science Researcher",
+        "img": "elissa.webp"
+    },
+    {
+        "name": "Michael Wrana",
+        "role": "Front-End Developer",
+        "img": "wrana.webp"
+    },
+    {
+        "name": "Donal Lynagh",
+        "role": "Business Operations",
+        "img": "donal.webp"
+    },
+    {
+        "name": "Justine Kuczera",
+        "role": "Business Operations",
+        "img": "justine.webp"
+    },
+    {
+        "name": "Quantum Hu",
+        "role": "Hardware Designer",
+        "img": "quantum.webp"
+    },
+    {
+        "name": "Carter Conboy",
+        "role": "Front-End Developer",
+        "img": "carter.webp"
+    },
+    {
+        "name": "Joshua Sass-Gregoire",
+        "role": "Mechanical Designer",
+        "img": "joshua.webp"
+    },
+]
+
+partner_data = [
+    {
+        "url": "https://www.bmahydroponics.com/",
+        "img": "bma.webp"
+    },
+    {
+        "url": "https://zipgrow.com/",
+        "img": "zipgrow.webp"
+    },
+    {
+        "url": "https://www.igrow.news/",
+        "img": "igrow.webp"
+    },
+    {
+        "url": "https://www.engsoc.queensu.ca/get-involved/clubs/",
+        "img": "engsoc.webp"
+    },
+    {
+        "url": "http://simbl.ca/",
+        "img": "simbl.webp"
+    },
+    {
+        "url": "https://aerok.ca/",
+        "img": "aerok.webp"
+    },
+]
+
+hiring_data = [
+    {
+        "role": "Mechanical Designer",
+        "img": "Gears_Icon.webp"
+    },
+    {
+        "role": "Software Developer",
+        "img": "Laptop_Icon.webp"
+    },
+    {
+        "role": "Electrical Engineer",
+        "img": "circuit.webp"
+    },
+    {
+        "role": "Plant Science Researcher",
+        "img": "premiumProduce.webp"
+    },
+    {
+        "role": "Business Operations Manager",
+        "img": "ourTeam.webp"
+    },
+]
--- qvft/views.py
+++ qvft/views.py
@@ -1,135 +1,29 @@
 from qvft import app
+from qvft import data
 from flask import render_template
 
-navigation_data = [
-    {
-        "caption": "What We Do",
-        "href": "/about"
-    },
-    {
-        "caption": "Our Team",
-        "href": "/team"
-    },
-    {
-        "caption": "Sponsorship",
-        "href": "/sponsors"
-    },
-    {
-        "caption": "Hiring",
-        "href": "/hiring"
-    }
-]
 
-team_data = [
-    {
-        "name": "Patrick Singal",
-        "role": "Manager",
-        "url": "patrick.webp"
-    },
-    {
-        "name": "Chris Molloy",
-        "role": "Database Developer",
-        "url": "chris.webp"
-    },
-    {
-        "name": "Iain Headrick",
-        "role": "Mechanical Designer",
-        "url": "iain.webp"
-    },
-    {
-        "name": "Julia Mackey",
-        "role": "Business Operations",
-        "url": "julia.webp"
-    },
-    {
-        "name": "Ryan Power",
-        "role": "Hardware Designer",
-        "url": "ryan.webp"
-    },
-    {
-        "name": "Elissa Wong",
-        "role": "Plant Science Researcher",
-        "url": "elissa.webp"
-    },
-    {
-        "name": "Michael Wrana",
-        "role": "Front-End Developer",
-        "url": "wrana.webp"
-    },
-    {
-        "name": "Donal Lynagh",
-        "role": "Business Operations",
-        "url": "donal.webp"
-    },
-    {
-        "name": "Justine Kuczera",
-        "role": "Business Operations",
-        "url": "justine.webp"
-    },
-    {
-        "name": "Quantum Hu",
-        "role": "Hardware Designer",
-        "url": "quantum.webp"
-    },
-    {
-        "name": "Carter Conboy",
-        "role": "Front-End Developer",
-        "url": "carter.webp"
-    },
-    {
-        "name": "Joshua Sass-Gregoire",
-        "role": "Mechanical Designer",
-        "url": "joshua.webp"
-    },
-]
-
-partner_data = [
-    {
-        "url": "https://www.bmahydroponics.com/",
-        "img": "bma.webp"
-    },
-    {
-        "url": "https://zipgrow.com/",
-        "img": "zipgrow.webp"
-    },
-    {
-        "url": "https://www.igrow.news/",
-        "img": "igrow.webp"
-    },
-    {
-        "url": "https://www.engsoc.queensu.ca/get-involved/clubs/",
-        "img": "engsoc.webp"
-    },
-    {
-        "url": "http://simbl.ca/",
-        "img": "simbl.webp"
-    },
-    {
-        "url": "https://aerok.ca/",
-        "img": "aerok.webp"
-    },
-]
 
 @app.route('/')
 def index():
-    return render_template('home.html', navigation=navigation_data, partner_data=partner_data)
+    return render_template('home.html', navigation=data.nav_data, partner_data=data.partner_data)
 
 
 @app.route('/about/')
 def about():
-    return render_template('about.html', navigation=navigation_data)
+    return render_template('about.html', navigation=data.nav_data)
 
 
 @app.route('/team/')
 def team():
-    return render_template('team.html', navigation=navigation_data, team_data=team_data)
+    return render_template('team.html', navigation=data.nav_data, team_data=data.team_data)
 
 
 @app.route('/sponsors/')
 def sponsors():
-    return render_template('sponsors.html', navigation=navigation_data)
+    return render_template('sponsors.html', navigation=data.nav_data)
 
 
 @app.route('/hiring/')
 def hiring():
-    return render_template('hiring.html', navigation=navigation_data)
+    return render_template('hiring.html', navigation=data.nav_data, hiring_data=data.hiring_data)
