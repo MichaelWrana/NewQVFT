92130b044631d5b4d48edb981341f37147204b55
MichaelWrana <wranamichael@gmail.com>
2021-12-11 00:15:03 -0500

    fixed spacing

--- qvft/views.py
+++ qvft/views.py
@@ -5,24 +5,32 @@ from flask import render_template
 
 @app.route('/')
 def index():
-    return render_template('home.html', navigation=data.nav_data, partner_data=data.partner_data)
+    return render_template('home.html',
+                           navigation=data.nav_data,
+                           partner_data=data.partner_data)
 
 
 @app.route('/about/')
 def about():
-    return render_template('about.html', navigation=data.nav_data)
+    return render_template('about.html',
+                           navigation=data.nav_data)
 
 
 @app.route('/team/')
 def team():
-    return render_template('team.html', navigation=data.nav_data, team_data=data.team_data)
+    return render_template('team.html',
+                           navigation=data.nav_data,
+                           team_data=data.team_data)
 
 
 @app.route('/sponsors/')
 def sponsors():
-    return render_template('sponsors.html', navigation=data.nav_data)
+    return render_template('sponsors.html',
+                           navigation=data.nav_data)
 
 
 @app.route('/hiring/')
 def hiring():
-    return render_template('hiring.html', navigation=data.nav_data, hiring_data=data.hiring_data)
+    return render_template('hiring.html',
+                           navigation=data.nav_data,
+                           hiring_data=data.hiring_data)
